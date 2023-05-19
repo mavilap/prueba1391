c8e5b59031a10d739fad8c3ab52353dff645c7b8
DESKTOP-PUN0B2F\Mario Avila <marioavi@gmail.com>
2023-05-19 04:02:12 -0500

    react  vite
    vite mas react

---
+++ src/App.jsx
@@ -0,0 +1,35 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <div>
+        <a href="https://vitejs.dev" target="_blank">
+          <img src={viteLogo} className="logo" alt="Vite logo" />
+        </a>
+        <a href="https://react.dev" target="_blank">
+          <img src={reactLogo} className="logo react" alt="React logo" />
+        </a>
+      </div>
+      <h1>Mi proyecto de grupo 1</h1>
+      <div className="card">
+        <h1> Integrantes</h1>
+        <h4>Mario Kempes</h4>
+        <h4>Diego Maradona</h4>
+        <a href="index.html">Ir al proyecto</a>
+
+        
+      </div>
+      <p className="read-the-docs">
+        Click on the Vite and React logos to learn more
+      </p>
+    </>
+  )
+}
+
+export default App
